e98552e2528565ae6c06a7f656b4e55cda20c5fe
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-01-16 18:23:23 +0100

    implement Routes, Navigate and NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
+import { PageOne } from "components/pages/PageOne";
+import { PageTwo } from "components/pages/PageTwo";
+import { PageThree } from "components/pages/PageThree";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
+import { Error404 } from "components/pages/Error404";
 
 
 function App() {
-    return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    Здесь будет навигация
-                </div>
-                <div className={styles.content}>
-                    Здесь будут кроссовки
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+  return (
+    <div>
+      <div className={ styles.header }><h1>HEADER</h1></div>
+      <div className={ styles.body }>
+        <nav className={ styles.nav }>
+          <ul>
+            <li><NavLink to="/page1">Page 1</NavLink></li>
+            <li><NavLink to="/page2">Page 2</NavLink></li>
+            <li><NavLink to="/page3">Page 3</NavLink></li>
+          </ul>
+        </nav>
+        <div className={ styles.content }>
+          <Routes>
+            <Route path="/" element={ <Navigate to="/page1" /> } />
+            <Route path="/page1" element={ <PageOne /> } />
+            <Route path="/page2" element={ <PageTwo /> } />
+            <Route path="/page3" element={ <PageThree /> } />
+            <Route path="/page/error" element={ <Error404 /> } />
+            <Route path="/*" element={ <Navigate to="/page/error" /> } />
+          </Routes>
         </div>
-    );
+      </div>
+      <div className={ styles.footer }>abibas 2023</div>
+    </div>
+  );
 }
 
 
